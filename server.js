7e190bc82f0e18599a6a52c4128e71bfe04ede6c
jeonwon <jeonwon@naver.com>
2022-10-31 17:22:42 +0900

    서버js 만들어 동작하는거 확인후 커밋함

---
+++ server.js
@@ -0,0 +1,23 @@
+const express =require('express');
+const app = express();
+
+app.get('/bobae', function(req,res){
+    res.send('hello bobae world!');
+});
+
+app.listen(8000,()=>{
+    console.log('connect on 8000');
+});
+
+app.get('/hi', function(req,res){
+    res.send('hellow my name is HI~');
+});
+
+app.get('/paju', function(req,res){
+  
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/me', (req,res)=>{
+    res.sendFile(__dirname+'/me.html');
+})
