7364c2c84433575a65147e8b4d36d47fbdaca9f9
(전원식) <(jeonwons@hanmail.net)>
2023-11-16 16:58:33 +0900

    인덱스 파일 add  index.js

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const express =require('express');
+const express = require("express");
 const app = express();
 
-app.get('/bobae', function(req,res){
-    res.send('hello bobae world!');
+app.get("/bobae", function (req, res) {
+  res.send("hello bobae world!");
 });
 
-app.listen(8000,()=>{
-    console.log('connect on 8000');
+app.listen(3000, () => {
+  console.log("connect on 3000");
 });
 
-app.get('/hi', function(req,res){
-    res.send('hellow my name is HI~');
+app.get("/hi", function (req, res) {
+  res.send("hellow my name is HI~");
 });
 
-app.get('/paju', function(req,res){
-  
-    res.sendFile(__dirname + '/index.html');
+app.get("/paju", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/me', (req,res)=>{
-    res.sendFile(__dirname+'/me.html');
-})
+app.get("/me", (req, res) => {
+  res.sendFile(__dirname + "/me.html");
+});
